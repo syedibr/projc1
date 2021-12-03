a9294e7cc707510a3529e5a8effc9b575891eb93
syedibr <92871985+syedibr@users.noreply.github.com>
2021-12-03 08:51:59 +0530

    first commit

---
+++ merchants/src/main/java/com/ibu/merchants/repo/CompaniesRepo.java
@@ -0,0 +1,9 @@
+package com.ibu.merchants.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ibu.merchants.model.Companies;
+
+public interface CompaniesRepo extends CrudRepository<Companies,String> {
+
+}
---
+++ merchants/src/test/java/com/yaseen/merchants/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaseen.merchants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
