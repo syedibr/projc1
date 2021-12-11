52764479a2146c140fd44f79bf9544074248b610
syedibr <92871985+syedibr@users.noreply.github.com>
2021-12-11 12:34:13 +0530

    Delete merchants directory

--- merchants/src/main/java/com/ibu/merchants/repo/CompaniesRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ibu.merchants.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ibu.merchants.model.Companies;
-
-public interface CompaniesRepo extends CrudRepository<Companies,String> {
-
-}
