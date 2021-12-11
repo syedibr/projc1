a02e03f72d3dcdeb48f14785ff104f7fc10ec376
syedibr <92871985+syedibr@users.noreply.github.com>
2021-12-11 12:32:25 +0530

    Delete deals directory

--- deals/src/main/java/com/ibu/deals/repo/DealsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ibu.deals.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ibu.deals.model.Deals;
-
-public interface DealsRepo extends CrudRepository<Deals,String>{
-
-}
